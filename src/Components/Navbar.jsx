b857ed11b845a2b1e025e2015b829bc552ae8503
jimena-exe <jimena.alvarezva@amigo.edu.co>
2024-12-04 22:51:54 -0500

    principios del context: falta arreglar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { createContext } from "react";
 import { Link } from "react-router-dom";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
+const ContextoDelTema = createContext("light");
+
+function ProveedorDelTema({ children }) {
+  const [tema, setTema] = useState("light");
+
+  const cambiarTema = () => {
+    setTema((temaPrevio) => (temaPrevio === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ContextoDelTema.Provider value={{ tema, cambiarTema }}>
+      {children}
+    </ContextoDelTema.Provider>
+  );
+}
+
+//Componente
 
 const Navbar = () => {
+  const { tema, cambiarTema } = useContext(ContextoDelTema);
+
   return (
-    <nav>
+    <nav className={tema}>
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
       <Link to="/home">Home</Link>
       <Link to="/favs">Destacados</Link>
       <Link to="/contact">Contacto</Link>
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
+      <button onClick={cambiarTema}>Change theme</button>
     </nav>
   );
 };
